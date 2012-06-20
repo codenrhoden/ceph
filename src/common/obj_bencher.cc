62c4043f9f8373fbe1852318b857f2f45a89e4db
Mike Ryan <mike.ryan@inktank.com>
2012-06-20 12:59:04 -0700

    rados_bench: remove all benchmark files matching a prefix
    This is a fallback for when a user wishes to delete ALL benchmark files
    matching a particular prefix. In the fast case, a metadata file tells us
    enough to quickly delete the files in parallel. This is the slow case,
    where each file's name must be checked against the prefix.
    
    Signed-off-by: Mike Ryan <mike.ryan@inktank.com>

--- src/common/obj_bencher.cc
+++ src/common/obj_bencher.cc
@@ -680,8 +680,15 @@ int ObjBencher::clean_up(const std::string& prefix, int concurrentios) {
   snprintf(metadata_name, 128, "%s_metadata", prefix.c_str());
 
   r = fetch_bench_metadata(metadata_name, object_size, num_objects, prevPid);
-  if (r != 1) return r;
-  // if this file is not found we should try to do a linear search on the prefix
+  if (r != 1) {
+    // if the metadata file is not found we should try to do a linear search on the prefix
+    if (r == -2) {
+      return clean_up_slow(prefix);
+    }
+    else {
+      return r;
+    }
+  }
 
   r = clean_up(num_objects, prevPid, concurrentios);
   if (r != 0) return r;
@@ -822,4 +829,23 @@ int ObjBencher::clean_up(int num_objects, int prevPid, int concurrentios) {
   return -5;
 }
 
+int ObjBencher::clean_up_slow(const std::string& prefix) {
+  std::list<string> objects;
+  int removed = 0;
 
+  out(cout) << "Warning: using slow linear search with sync remove" << std::endl;
+
+  while (get_objects(objects, 20)) {
+    std::list<std::string>::const_iterator i = objects.begin();
+    for ( ; i != objects.end(); ++i) {
+      if (i->substr(0, prefix.size()) == prefix) {
+        sync_remove(*i);
+        ++removed;
+      }
+    }
+  }
+
+  out(cout) << "Removed " << removed << " object" << (removed != 1 ? "s" : "") << std::endl;
+
+  return 0;
+}
